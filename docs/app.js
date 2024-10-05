0b381d81203dc47aec6e1f74c493f5286c4029c8
Nonia Acosta <nonia.acosta@americana.edu.py>
2024-10-05 08:59:32 -0400

    desafio

---
+++ docs/app.js
@@ -0,0 +1,49 @@
+// Selecciona los elementos del DOM.
+const inputRojo = document.getElementById('rojo');
+const inputVerde = document.getElementById('verde');
+const inputAzul = document.getElementById('azul');
+
+const textoRojo = document.getElementById('texto-rojo');
+const textoVerde = document.getElementById('texto-verde');
+const textoAzul = document.getElementById('texto-azul');
+
+// Valores iniciales para definir el color RGB.
+let rojo = inputRojo.value;
+let verde = inputVerde.value;
+let azul = inputAzul.value;
+
+// Muestra los valores iniciales en los elementos <p>.
+textoRojo.textContent = rojo;
+textoVerde.textContent = verde;
+textoAzul.textContent = azul;
+
+// Función para actualizar el color de fondo de la página.
+function actualizarColor(rojo, verde, azul) {
+  document.body.style.backgroundColor = 'rgb(' + rojo + ',' + verde + ',' + azul + ')';
+  //rgb(rojo, verde, azul);
+}
+
+// Agrega un event listener para el input de Rojo.
+inputRojo.addEventListener('change', (e) => {
+  // Actualiza el valor de la variable y el texto.
+  // Llama a la función para actualizar el color de fondo.
+  console.log(inputRojo.value)
+  document.getElementById("texto-rojo").innerHTML = inputRojo.value;
+  actualizarColor(inputRojo.value, inputVerde.value, inputAzul.value);
+});
+
+// Agrega un event listener para el input de Verde.
+inputVerde.addEventListener('change', (e) => {
+  // Actualiza el valor de la variable y el texto.
+  // Llama a la función para actualizar el color de fondo.
+  document.getElementById("texto-verde").innerHTML = inputVerde.value;
+  actualizarColor(inputRojo.value, inputVerde.value, inputAzul.value);
+});
+
+// Agrega un event listener para el input de Azul.
+inputAzul.addEventListener('change', (e) => {
+  // Actualiza el valor de la variable y el texto.
+  // Llama a la función para actualizar el color de fondo.
+  document.getElementById("texto-azul").innerHTML = inputAzul.value;
+  actualizarColor(inputRojo.value, inputVerde.value, inputAzul.value);
+});
